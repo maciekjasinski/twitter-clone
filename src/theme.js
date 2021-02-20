399dfcc6f406dd2578ad401d53846ff180ef0f2f
maciekjasinski <maciek.mj00@gmail.com>
2021-02-20 16:56:41 +0100

    feat: add router and theme provider

---
+++ src/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+    colors: {
+        primary: 'rgba(29,161,242,1.00)',
+        white: 'rgb(255, 255, 255)',
+        black: 'rgb(15, 20, 25)',
+        gray: 'rgb(91, 112, 131)',
+        borderGray: 'rgb(196, 207, 214)'
+    }
+}
